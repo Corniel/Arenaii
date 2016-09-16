e279b082f447d3af34d42811dc9c49aeab8d7cbe
Corniel Nobel <mail@corniel.nl>
2016-09-16 13:43:40 +0200

    Not longer try to be CLSCompliant.

--- src/Shared/AssemblyInfo.Product.cs
+++ src/Shared/AssemblyInfo.Product.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
 
 [assembly: AssemblyProduct("Arenaii")]
 [assembly: AssemblyCopyright("Copyright © 2015-2016, Ad van der Hoeven, Corniel Nobel")]
