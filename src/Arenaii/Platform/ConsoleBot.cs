79d6bddded2c37b859d031a78b3fadc97e3c0e6f
Corniel Nobel <corniel@gmail.com>
2024-10-07 14:45:43 +0200

    Log standard output.

--- src/Arenaii/Platform/ConsoleBot.cs
+++ src/Arenaii/Platform/ConsoleBot.cs
@@ -9,138 +9,147 @@ namespace Arenaii.Platform;
 
 [DebuggerDisplay("Bot = {Bot}")]
 public sealed class ConsoleBot : IDisposable
-	{
-		public ConsoleBot(Bot bot)
-		{
-			Bot = bot;
-			if(AppConfig.LogDirectory != null)
-			{
-				var name = string.Format("{0:yyyy-MM-dd HH_mm_ss_fff}.log", Clock.Now());
-				var file = new FileInfo(Path.Combine(AppConfig.LogDirectory.FullName, bot.FullName, name));
-				if(!file.Directory!.Exists)
-				{
-					file.Directory.Create();
-				}
-				Writer = new StreamWriter(file.FullName, false);
-			}
-		}
-		public Bot Bot { get; }
+{
+    public ConsoleBot(Bot bot)
+    {
+        Bot = bot;
+        if (AppConfig.LogDirectory != null)
+        {
+            var name = string.Format("{0:yyyy-MM-dd HH_mm_ss_fff}.log", Clock.Now());
+            var file = new FileInfo(Path.Combine(AppConfig.LogDirectory.FullName, bot.FullName, name));
+            if (!file.Directory!.Exists)
+            {
+                file.Directory.Create();
+            }
+            Writer = new StreamWriter(file.FullName, false);
+        }
+    }
+    public Bot Bot { get; }
 
-		private readonly Stopwatch Timer = new();
+    private readonly Stopwatch Timer = new();
 
-		private readonly StreamWriter Writer;
+    private readonly StreamWriter Writer;
 
-		/// <summary>Start the timer for the bot.</summary>
-		public void Start() { Timer.Start(); }
-		
-		/// <summary>Stop the timer for the bot.</summary>
-		public void Stop() { Timer.Stop(); }
-		/// <summary>Get the elapsed milliseconds.</summary>
-		public TimeSpan Elapsed { get { return Timer.Elapsed; } }
+    /// <summary>Start the timer for the bot.</summary>
+    public void Start() { Timer.Start(); }
 
-		/// <summary>Did the bot time out?</summary>
-		public bool TimedOut { get; private set; }
-		
+    /// <summary>Stop the timer for the bot.</summary>
+    public void Stop() { Timer.Stop(); }
+    /// <summary>Get the elapsed milliseconds.</summary>
+    public TimeSpan Elapsed { get { return Timer.Elapsed; } }
+
+    /// <summary>Did the bot time out?</summary>
+    public bool TimedOut { get; private set; }
+
     public string Read(TimeSpan timeout)
-		{
-			Start();
-			using var tokenSource = new CancellationTokenSource();
-			var token = tokenSource.Token;
-			var task = Task.Factory.StartNew(process.StandardOutput.ReadLine, token);
-			if (!task.Wait((int)timeout.TotalMilliseconds, token))
-			{
-				process.Kill();
-				TimedOut = true;
-				return string.Empty;
-			}
-			Stop();
-			Log("Response: {0}", task.Result);
-			return task.Result;
-		}
+    {
+        Start();
+        using var tokenSource = new CancellationTokenSource();
+        var token = tokenSource.Token;
+        var task = Task.Factory.StartNew(process.StandardOutput.ReadLine, token);
+        if (!task.Wait((int)timeout.TotalMilliseconds, token))
+        {
+            process.Kill();
+            TimedOut = true;
+            return string.Empty;
+        }
+        Stop();
+        Log("Response: {0}", task.Result);
 
-		public void Write(string message)
-		{
-			process.StandardInput.WriteLine(message);
-			Log(message);
-		}
-		public void Write(string format, params object[] args)
-		{
-			process.StandardInput.WriteLine(format, args);
-			Log(format, args);
-		}
+        return task.Result;
+    }
 
-		private void Log(string message)
-		{
-			if(Writer != null)
-			{
-				Writer.WriteLine(message);
-				Writer.Flush();
-			}
-		}
-		private void Log(string format, params object[] args)
-		{
-			if (Writer != null)
-			{
-				Writer.WriteLine(format, args);
-				Writer.Flush();
-			}
-		}
+    public void Write(string message)
+    {
+        process.StandardInput.WriteLine(message);
+        Log(message);
+    }
+    public void Write(string format, params object[] args)
+    {
+        process.StandardInput.WriteLine(format, args);
+        Log(format, args);
+    }
 
-		/// <summary>The process required to run the bot.</summary>
-		protected Process process;
+    private void Log(string message)
+    {
+        if (Writer != null)
+        {
+            Writer.WriteLine(message);
+            Writer.Flush();
+        }
+    }
+    private void Log(string format, params object[] args)
+    {
+        if (Writer != null)
+        {
+            Writer.WriteLine(format, args);
+            Writer.Flush();
+        }
+    }
 
-		protected static Process CreateProcess(FileInfo exe)
-		{
-            var p = new Process();
-			p.StartInfo.WorkingDirectory = exe.Directory!.FullName;
-			p.StartInfo.FileName = exe.FullName;
-			p.StartInfo.UseShellExecute = false;
-			//p.StartInfo.CreateNoWindow = true;
-			p.StartInfo.RedirectStandardInput = true;
-			p.StartInfo.RedirectStandardOutput = true;
-			p.Start();
-			return p;
-		}
+    /// <summary>The process required to run the bot.</summary>
+    protected Process process;
 
-		public static ConsoleBot Create(Bot bot)
-		{
-			var console = new ConsoleBot(bot)
-			{
-				process = CreateProcess(bot.Location),
-			};
-			return console;
-		}
+    protected Process CreateProcess(FileInfo exe)
+    {
+        var p = new Process();
+        p.StartInfo.WorkingDirectory = exe.Directory!.FullName;
+        p.StartInfo.FileName = exe.FullName;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = true;
+        p.StartInfo.RedirectStandardInput = true;
+        p.StartInfo.RedirectStandardOutput = true;
+        p.StartInfo.RedirectStandardError = true;
+        p.ErrorDataReceived += ProcessStandardError;
+        p.Start();
+        return p;
+    }
 
-		#region IDisposable
+    private void ProcessStandardError(object sender, DataReceivedEventArgs e)
+    {
+        if (e.Data is { Length: > 0 })
+        {
+            Log(e.Data);
+        }
+    }
 
-		/// <summary>Dispose the console platform.</summary>
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+    public static ConsoleBot Create(Bot bot)
+    {
+        var console = new ConsoleBot(bot);
+        console.process = console.CreateProcess(bot.Location);
+        return console;
+    }
 
-		/// <summary>Dispose the console platform.</summary>
-		protected void Dispose(bool disposing)
-		{
-			if (!m_IsDisposed)
-			{
-				if (disposing && process != null)
-				{
-					process.Dispose();
-				}
-				if(disposing && Writer != null)
-				{
-					Writer.Dispose();
-				}
-				m_IsDisposed = true;
-			}
-		}
+    #region IDisposable
 
-		/// <summary>Destructor</summary>
-		~ConsoleBot() { Dispose(false); }
+    /// <summary>Dispose the console platform.</summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 
-		private bool m_IsDisposed;
+    /// <summary>Dispose the console platform.</summary>
+    protected void Dispose(bool disposing)
+    {
+        if (!m_IsDisposed)
+        {
+            if (disposing && process != null)
+            {
+                process.Dispose();
+            }
+            if (disposing && Writer != null)
+            {
+                Writer.Dispose();
+            }
+            m_IsDisposed = true;
+        }
+    }
 
-		#endregion
-	}
+    /// <summary>Destructor</summary>
+    ~ConsoleBot() { Dispose(false); }
+
+    private bool m_IsDisposed;
+
+    #endregion
+}
